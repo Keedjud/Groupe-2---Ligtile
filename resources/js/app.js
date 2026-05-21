73bd4306d8d6e6fa065c2f3643e331aa02cbfbbc
El-cacahu3te <elia.nicolo@outlook.com>
2026-05-21 11:47:49 +0200

    mise en place vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
-//
+import './bootstrap';
+import { createApp } from 'vue';
+import Home from './components/Home.vue';
+
+const app = createApp({});
+app.component('home-component', Home);
+app.mount('#app');
