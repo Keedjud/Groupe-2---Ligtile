2c151d3f3f3e51ce9f96fc2f1cfd2b3caddb8f0d
Loic <loic63130@gmail.com>
2026-05-29 15:44:43 +0200

    Splash and session system (only on the splash page)

--- resources/js/public/App.vue
+++ resources/js/public/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import PublicDefaultLayout from './layouts/PublicDefaultLayout.vue'
+import SplashScreen from './components/SplashScreen.vue'
 </script>
 
 <template>
+  <SplashScreen />
   <PublicDefaultLayout />
 </template>
