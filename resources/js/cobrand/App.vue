48bcd8674ea9d0006674e31a0cfa82a103a041af
Loic <loic63130@gmail.com>
2026-06-04 20:45:53 +0200

    routage et pages cobrand

--- resources/js/cobrand/App.vue
+++ resources/js/cobrand/App.vue
@@ -1,8 +1,28 @@
-<template>
-</template>
-
 <script setup>
+import CobrandLayout from "./layouts/CobrandLayout.vue";
+import { useHashRoute } from "@/composables/router";
+
+import Accueil from "./views/Accueil.vue";
+import Prevention from "./views/Prevention.vue";
+import Quiz from "./views/Quiz.vue";
+import Redirect from "./views/Redirect.vue";
+
 defineProps({
     collecteId: { type: String, required: true },
 });
+
+const routes = [
+    { hash: "#/accueil", key: "accueil", component: Accueil },
+    { hash: "#/prevention", key: "prevention", component: Prevention },
+    { hash: "#/quiz", key: "quiz", component: Quiz },
+    { hash: "#/inscription", key: "inscription", component: Redirect },
+];
+
+const { currentComponent, currentRoute } = useHashRoute(routes);
 </script>
+
+<template>
+    <CobrandLayout :current="currentRoute.key">
+        <component :is="currentComponent" />
+    </CobrandLayout>
+</template>
