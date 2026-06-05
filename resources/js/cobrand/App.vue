238e31ad8cd2ef6f59a3ee627011231020a0aa03
El-cacahu3te <elia.nicolo@outlook.com>
2026-06-05 17:13:23 +0200

    chance

--- resources/js/cobrand/App.vue
+++ resources/js/cobrand/App.vue
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<template>
-    <router-view />
-</template>
-
-=======
->>>>>>> develop
-<script setup>
-import { watch } from "vue";
-import { useHashRoute } from "@/composables/router";
-import { initSession, useCobrandSession } from "./composables/useCobrandSession";
-
-import Accueil    from "./views/Accueil.vue";
-import Prevention from "./views/Prevention.vue";
-import Quiz       from "./views/Quiz.vue";
-import Redirect   from "./views/Redirect.vue";
-
-const props = defineProps({
-    collecteId: { type: String, required: true },
-<<<<<<< HEAD
-})
-=======
 <script setup>
 import { ref, onMounted } from 'vue'
 import { useFetchApi } from '@/composables/api/useFetchApi'
@@ -48,7 +25,6 @@ const routes = [
 ]
 
 const { currentComponent, currentRoute } = useHashRoute(routes)
->>>>>>> feature/dashboard
 </script>
 
 <template>
@@ -58,37 +34,4 @@ const { currentComponent, currentRoute } = useHashRoute(routes)
       <component :is="currentComponent" :collection="collection" />
     </main>
   </div>
-=======
-});
-
-const { theme } = useCobrandSession();
-initSession(props.collecteId);
-
-// Injection des couleurs de l'entreprise comme variables CSS sur :root.
-// Toutes les vues cobrand peuvent ensuite utiliser var(--cobrand-primary) etc.
-watch(
-    theme,
-    (t) => {
-        if (!t) return;
-        const style = document.documentElement.style;
-        Object.entries(t).forEach(([key, val]) => {
-            style.setProperty(`--cobrand-${key.replace(/_/g, "-")}`, val);
-        });
-    },
-    { immediate: true },
-);
-
-const routes = [
-    { hash: "#/accueil",     key: "accueil",     component: Accueil },
-    { hash: "#/prevention",  key: "prevention",  component: Prevention },
-    { hash: "#/quiz",        key: "quiz",         component: Quiz },
-    { hash: "#/inscription", key: "inscription",  component: Redirect },
-];
-
-const { currentComponent } = useHashRoute(routes);
-</script>
-
-<template>
-    <component :is="currentComponent" />
->>>>>>> develop
 </template>
