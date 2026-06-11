6fe48db3984b62a8aeaa04a1234867cee8510763
El-cacahu3te <elia.nicolo@outlook.com>
2026-06-11 17:05:11 +0200

    bouton baromètre hug

--- resources/js/cobrand/views/Accueil.vue
+++ resources/js/cobrand/views/Accueil.vue
@@ -237,12 +237,13 @@ const bloodTypes = [
                         </div>
                     </div>
                 </div>
-
+                <a href="https://www.hug.ch/don-du-sang/barometre-don-du-sang-geneve">
                 <button
 
                     class="mt-6 block w-max mx-auto rounded-full border border-[var(--color-light-palette-gray)] bg-[var(--color-light-palette-white)] px-4 py-2 text-regular text-[color:var(--accent)] shadow-[0px_0px_4px_0px_rgba(0,0,0,0.25)] transition hover:-translate-y-0.5 hover:bg-[var(--color-beige-25)] hover:shadow-md sm:mx-0">
-                    <a href="https://www.hug.ch/don-du-sang/barometre-don-du-sang-geneve">Consulter le baromètre complet des HUG</a>
+                   Consulter le baromètre complet des HUG
                 </button>
+            </a>
             </section>
 
 
