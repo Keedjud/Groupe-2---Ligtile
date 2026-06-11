5007222fc26b1804f1e7c047f3a7a42141e14d63
Keedjud <inoewenger@gmail.com>
2026-06-11 11:46:58 +0200

    fix(controller): update size range thresholds for company classification

--- app/Http/Controllers/Api/v1/LabelCompanyController.php
+++ app/Http/Controllers/Api/v1/LabelCompanyController.php
@@ -72,11 +72,12 @@ class LabelCompanyController extends Controller
      */
     private function sizeRange(string $size): array
     {
+        // Seuils alignés sur Company::getSizeLabelAttribute()
         return match ($size) {
-            'grande'  => [250, 1_000_000],
-            'moyenne' => [50, 249],
-            'petite'  => [10, 49],
-            default   => [0, 9],
+            'grande'  => [1000, 1_000_000],
+            'moyenne' => [500, 999],
+            'petite'  => [100, 499],
+            default   => [0, 99],
         };
     }
 }
